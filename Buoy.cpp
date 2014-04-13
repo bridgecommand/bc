4f4cf9b0bb5ce961271628d37c0141783446a3d0
James Packer <james@bridgecommand.co.uk>
2014-04-13 21:35:21 +0100

    Get buoy filename and scale information from each buoy model's buoy.ini file.

--- Buoy.cpp
+++ Buoy.cpp
@@ -4,10 +4,11 @@
 
 using namespace irr;
 
-Buoy::Buoy(const irr::io::path& filename, const irr::core::vector3df& location, irr::scene::ISceneManager* smgr)
+Buoy::Buoy(const irr::io::path& filename, const irr::core::vector3df& location, const irr::f32 scale, irr::scene::ISceneManager* smgr)
 {
     scene::IMesh* buoyMesh = smgr->getMesh(filename);
     buoy = smgr->addMeshSceneNode(buoyMesh,0,-1,location);
+    buoy->setScale(core::vector3df(scale,scale,scale));
     buoy->setMaterialFlag(video::EMF_LIGHTING, false);
 }
 
--- SimulationModel.cpp
+++ SimulationModel.cpp
@@ -46,14 +46,34 @@ SimulationModel::SimulationModel(IrrlichtDevice* dev, video::IVideoDriver* drv, 
             for(u32 i=1;i<=numberOfBuoys;i++)
             {
                 //Get buoy type and construct filename (FIXME: In due course, the buoy constructor should be given the path to the buoy folder, and use this to look in the local buoy.ini file for the model filename and scaling.
-                std::string buoyFilename = "Models/Buoy/"; //FIXME: Use proper path handling
-                buoyFilename.append(IniFile::iniFileToString("Buoy.ini",IniFile::enumerate1("Type",i)));
-                buoyFilename.append("/buoy.x");
+                std::string buoyName = IniFile::iniFileToString("Buoy.ini",IniFile::enumerate1("Type",i));
+
+                //Load from buoy.ini file if it exists
+                std::string buoyIniFilename = "Models/Buoy/";
+                buoyIniFilename.append(buoyName);
+                buoyIniFilename.append("/buoy.ini");
+
+                //get filename from ini file (or empty string if file doesn't exist)
+                std::string buoyFileName = IniFile::iniFileToString(buoyIniFilename,"FileName");
+                if (buoyFileName=="") {
+                    buoyFileName = "buoy.x"; //Default if not set
+                }
+                //get scale factor from ini file (or zero if not set - assume 1)
+                f32 buoyScale = IniFile::iniFileTof32(buoyIniFilename,"Scalefactor");
+                if (buoyScale==0.0) {
+                    buoyScale = 1.0; //Default if not set
+                }
+
+                std::string buoyFullPath = "Models/Buoy/"; //FIXME: Use proper path handling
+                buoyFullPath.append(buoyName);
+                buoyFullPath.append("/");
+                buoyFullPath.append(buoyFileName);
+
                 //Get buoy position
                 f32 buoyX = longToX(IniFile::iniFileTof32("Buoy.ini",IniFile::enumerate1("Long",i)));
                 f32 buoyZ = latToZ(IniFile::iniFileTof32("Buoy.ini",IniFile::enumerate1("Lat",i)));
                 //Create buoy and load into vector
-                buoys.push_back(Buoy (buoyFilename.c_str(),core::vector3df(buoyX,0.0f,buoyZ),smgr));
+                buoys.push_back(Buoy (buoyFullPath.c_str(),core::vector3df(buoyX,0.0f,buoyZ),buoyScale,smgr));
             }
         }
         //End: Will be moved into a Buoys object(?)
