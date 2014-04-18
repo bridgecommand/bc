202e24ecbf0ff71075687a0f651b6b81016f8ae5
James Packer <james@bridgecommand.co.uk>
2014-04-18 15:31:30 +0100

    Small adjustment to water level (so mean is around zero, and amplitude is slightly reduced.)

--- Water.cpp
+++ Water.cpp
@@ -13,8 +13,8 @@ Water::Water(irr::scene::ISceneManager* smgr, irr::video::IVideoDriver* driver)
                        core::dimension2d<f32>(0,0),
                        core::dimension2d<f32>(10,10));
 
-    waterNode = smgr->addWaterSurfaceSceneNode(waterMesh->getMesh(0), 0.5f, 300.0f, 10.0f);
-    waterNode->setPosition(core::vector3df(0,-2*0.5f,0));
+    waterNode = smgr->addWaterSurfaceSceneNode(waterMesh->getMesh(0), 0.25f, 300.0f, 10.0f);
+    waterNode->setPosition(core::vector3df(0,-0.25f,0));
 
     waterNode->setMaterialTexture(0, driver->getTexture("media/water.bmp"));
     waterNode->setMaterialFlag(video::EMF_LIGHTING, false);
