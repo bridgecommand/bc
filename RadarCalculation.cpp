72e8d34eebb44032138e4df6ebee277cffd2ee20
James Packer <james@bridgecommand.co.uk>
2014-05-01 23:42:07 +0100

    Proof of principle implementation of terrain scanning radar.

--- RadarCalculation.cpp
+++ RadarCalculation.cpp
@@ -34,19 +34,36 @@ void RadarCalculation::updateRadarCalculation(irr::video::IImage * radarImage, c
     if (radarImage->getDimension().Height != bitmapWidth)
         {return;} //Check image is square, and return without action if not
 
-    for(int i = 0;i<bitmapWidth;i++) { //FIXME: Hardcoded to work with 64px, should check size of the radarImage
-        for(int j=0;j<bitmapWidth;j++) {
+    //Scan out
+    for (int scanAngle = 0; scanAngle <360; scanAngle+=1) {
+        f32 scanSlope = 0.0; //Slope at start of scan
+        f32 cellLength = 50;
+        for (int currentStep = 1; currentStep<bitmapWidth/2; currentStep++) {
             core::vector3df position = ownShip.getPosition();
-            f32 localPosX = ((float)i-(float)(bitmapWidth-1)/2.0)*50.0 + position.X; //Distance from ship (Hardcoded to 50m/px)
-            f32 localPosZ = ((float)(bitmapWidth-1)/2.0-(float)j)*50.0 + position.Z;
-            s32 pixelColour = terrain.getHeight(localPosX,localPosZ);
-            if (pixelColour > 255)
-                {pixelColour = 255;}
-            if (pixelColour < 0)
-                {pixelColour = 255;}
-            pixelColour = 255-pixelColour;
-            radarImage->setPixel(i,j,video::SColor(255,pixelColour,pixelColour,0));
+            f32 localPosX = position.X + cellLength*currentStep*sin(scanAngle*core::DEGTORAD); //Distance from ship
+            f32 localPosZ = position.Z + cellLength*currentStep*cos(scanAngle*core::DEGTORAD);
+            s32 pixelX = (float)(bitmapWidth)/2.0 + currentStep*sin(scanAngle*core::DEGTORAD);
+            s32 pixelY = (float)(bitmapWidth)/2.0 - currentStep*cos(scanAngle*core::DEGTORAD);
+
+            f32 localSlope = terrain.getHeight(localPosX,localPosZ)/(cellLength*currentStep);
+            f32 slopeChange = localSlope - scanSlope;
+            if (slopeChange>0) {
+                scanSlope = localSlope; //Highest so far on scan
+            }
+
+            if (pixelX >= 0 && pixelX < bitmapWidth && pixelY >= 0 && pixelY < bitmapWidth ) {
+
+                s32 pixelColour=0;
+
+                if (slopeChange > 0) {
+                    pixelColour = 255;
+                }
+
+                radarImage->setPixel(pixelX,pixelY,video::SColor(255,pixelColour,pixelColour,0));
+            }
+
         }
     }
+
     radarImage->setPixel(bitmapWidth/2,bitmapWidth/2,video::SColor(255,255,0,0)); //'centre' point //Fixme: Remove this from final, as it isn't exactly in centre
 }
