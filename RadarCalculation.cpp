487d5e5aad2d5fec59a91003057e74509e8994a1
James Packer <james@bridgecommand.co.uk>
2014-05-01 07:51:18 +0100

    Simple changes to radar calculation to remove image size hard-coding.

--- RadarCalculation.cpp
+++ RadarCalculation.cpp
@@ -26,12 +26,19 @@ void RadarCalculation::loadRadarCalculation()
 
 void RadarCalculation::updateRadarCalculation(irr::video::IImage * radarImage, const Terrain& terrain, const OwnShip& ownShip)
 {
+    //Fill with background colour
     radarImage->fill(video::SColor(255, 0, 0, 255));
-    for(int i = 0;i<64;i++) { //FIXME: Hardcoded to work with 64px, should check size of the radarImage
-        for(int j=0;j<64;j++) {
+
+    //Get image size
+    int bitmapWidth = radarImage->getDimension().Width;
+    if (radarImage->getDimension().Height != bitmapWidth)
+        {return;} //Check image is square, and return without action if not
+
+    for(int i = 0;i<bitmapWidth;i++) { //FIXME: Hardcoded to work with 64px, should check size of the radarImage
+        for(int j=0;j<bitmapWidth;j++) {
             core::vector3df position = ownShip.getPosition();
-            f32 localPosX = (i-32)*50.0 + position.X; //Hardcoded to 50m/px
-            f32 localPosZ = (32-j)*50.0 + position.Z;
+            f32 localPosX = ((float)i-(float)(bitmapWidth-1)/2.0)*50.0 + position.X; //Distance from ship (Hardcoded to 50m/px)
+            f32 localPosZ = ((float)(bitmapWidth-1)/2.0-(float)j)*50.0 + position.Z;
             s32 pixelColour = terrain.getHeight(localPosX,localPosZ);
             if (pixelColour > 255)
                 {pixelColour = 255;}
@@ -41,5 +48,5 @@ void RadarCalculation::updateRadarCalculation(irr::video::IImage * radarImage, c
             radarImage->setPixel(i,j,video::SColor(255,pixelColour,pixelColour,0));
         }
     }
-    radarImage->setPixel(32,32,video::SColor(255,255,0,0)); //'centre' point
+    radarImage->setPixel(bitmapWidth/2,bitmapWidth/2,video::SColor(255,255,0,0)); //'centre' point //Fixme: Remove this from final, as it isn't exactly in centre
 }
