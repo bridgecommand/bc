8c05f606275f02882b041dcdc6c3a3938369d7d7
James Packer <james@bridgecommand.co.uk>
2014-05-22 07:55:16 +0100

    Some reformatting of Buoy, and removing two if statements that weren't required, as the for loops wouldn't run if there weren't any objects to load.

--- Buoys.cpp
+++ Buoys.cpp
@@ -22,57 +22,52 @@ Buoys::~Buoys()
 
 void Buoys::load(const std::string& worldName, irr::scene::ISceneManager* smgr, SimulationModel* model)
 {
-        //get buoy.ini filename
-        std::string scenarioBuoyFilename = worldName;
-        scenarioBuoyFilename.append("/buoy.ini");
+    //get buoy.ini filename
+    std::string scenarioBuoyFilename = worldName;
+    scenarioBuoyFilename.append("/buoy.ini");
 
-        //get light.ini filename
-        std::string scenarioLightFilename = worldName;
-        scenarioLightFilename.append("/light.ini");
+    //get light.ini filename
+    std::string scenarioLightFilename = worldName;
+    scenarioLightFilename.append("/light.ini");
 
-        //Find number of buoys
-        u32 numberOfBuoys;
-        numberOfBuoys = IniFile::iniFileTou32(scenarioBuoyFilename,"Number");
-        if (numberOfBuoys > 0)
-        {
-            for(u32 currentBuoy=1;currentBuoy<=numberOfBuoys;currentBuoy++)
-            {
-                //Get buoy type and construct filename
-                std::string buoyName = IniFile::iniFileToString(scenarioBuoyFilename,IniFile::enumerate1("Type",currentBuoy));
-                //Get buoy position
-                f32 buoyX = model->longToX(IniFile::iniFileTof32(scenarioBuoyFilename,IniFile::enumerate1("Long",currentBuoy)));
-                f32 buoyZ = model->latToZ(IniFile::iniFileTof32(scenarioBuoyFilename,IniFile::enumerate1("Lat",currentBuoy)));
+    //Find number of buoys
+    u32 numberOfBuoys;
+    numberOfBuoys = IniFile::iniFileTou32(scenarioBuoyFilename,"Number");
+    for(u32 currentBuoy=1;currentBuoy<=numberOfBuoys;currentBuoy++) {
 
-                //Create buoy and load into vector
-                buoys.push_back(Buoy (buoyName.c_str(),core::vector3df(buoyX,0.0f,buoyZ),smgr));
+        //Get buoy type and construct filename
+        std::string buoyName = IniFile::iniFileToString(scenarioBuoyFilename,IniFile::enumerate1("Type",currentBuoy));
+        //Get buoy position
+        f32 buoyX = model->longToX(IniFile::iniFileTof32(scenarioBuoyFilename,IniFile::enumerate1("Long",currentBuoy)));
+        f32 buoyZ = model->latToZ(IniFile::iniFileTof32(scenarioBuoyFilename,IniFile::enumerate1("Lat",currentBuoy)));
 
-                //Load buoy light information from light.ini file if available
+        //Create buoy and load into vector
+        buoys.push_back(Buoy (buoyName.c_str(),core::vector3df(buoyX,0.0f,buoyZ),smgr));
 
-                //Find number of lights
-                u32 numberOfLights;
-                numberOfLights = IniFile::iniFileTou32(scenarioLightFilename,"Number");
-                if (numberOfLights > 0){
-                    //Run through lights, and check if any are associated with this buoy
-                    for (u32 currentLight=1;currentLight<=numberOfLights;currentLight++) {
-                        if (IniFile::iniFileTou32(scenarioLightFilename,IniFile::enumerate1("Buoy",currentLight)) ==currentBuoy ) {
-                            //Light on this buoy, add a light to the buoysLights vector in this location if required (FIXME: Make a child of the sea)
-                            f32 lightHeight = IniFile::iniFileTof32(scenarioLightFilename,IniFile::enumerate1("Height",currentLight));
-                            f32 lightR = IniFile::iniFileTou32(scenarioLightFilename,IniFile::enumerate1("Red",currentLight));
-                            f32 lightG = IniFile::iniFileTou32(scenarioLightFilename,IniFile::enumerate1("Green",currentLight));
-                            f32 lightB = IniFile::iniFileTou32(scenarioLightFilename,IniFile::enumerate1("Blue",currentLight));
-                            f32 lightRange = IniFile::iniFileTof32(scenarioLightFilename,IniFile::enumerate1("Range",currentLight));
-                            std::string lightSequence = IniFile::iniFileToString(scenarioLightFilename,IniFile::enumerate1("Sequence",currentLight));
-                            f32 lightStart = IniFile::iniFileTof32(scenarioLightFilename,IniFile::enumerate1("StartAngle",currentLight));
-                            f32 lightEnd = IniFile::iniFileTof32(scenarioLightFilename,IniFile::enumerate1("EndAngle",currentLight));
-                            lightRange = lightRange * M_IN_NM;
+        //Load buoy light information from light.ini file if available
+
+        //Find number of lights
+        u32 numberOfLights;
+        numberOfLights = IniFile::iniFileTou32(scenarioLightFilename,"Number");
+        //Run through lights, and check if any are associated with this buoy
+        for (u32 currentLight=1;currentLight<=numberOfLights;currentLight++) {
+            if (IniFile::iniFileTou32(scenarioLightFilename,IniFile::enumerate1("Buoy",currentLight)) ==currentBuoy ) {
+                //Light on this buoy, add a light to the buoysLights vector in this location if required (FIXME: Make a child of the sea)
+                f32 lightHeight = IniFile::iniFileTof32(scenarioLightFilename,IniFile::enumerate1("Height",currentLight));
+                f32 lightR = IniFile::iniFileTou32(scenarioLightFilename,IniFile::enumerate1("Red",currentLight));
+                f32 lightG = IniFile::iniFileTou32(scenarioLightFilename,IniFile::enumerate1("Green",currentLight));
+                f32 lightB = IniFile::iniFileTou32(scenarioLightFilename,IniFile::enumerate1("Blue",currentLight));
+                f32 lightRange = IniFile::iniFileTof32(scenarioLightFilename,IniFile::enumerate1("Range",currentLight));
+                std::string lightSequence = IniFile::iniFileToString(scenarioLightFilename,IniFile::enumerate1("Sequence",currentLight));
+                f32 lightStart = IniFile::iniFileTof32(scenarioLightFilename,IniFile::enumerate1("StartAngle",currentLight));
+                f32 lightEnd = IniFile::iniFileTof32(scenarioLightFilename,IniFile::enumerate1("EndAngle",currentLight));
+                lightRange = lightRange * M_IN_NM;
 
 
-                            buoysLights.push_back(NavLight (0,smgr,core::dimension2d<f32>(5, 5), core::vector3df(buoyX,lightHeight,buoyZ),video::SColor(255,lightR,lightG,lightB),lightStart,lightEnd,lightRange, lightSequence));
-                        }
-                    }
-                }
+                buoysLights.push_back(NavLight (0,smgr,core::dimension2d<f32>(5, 5), core::vector3df(buoyX,lightHeight,buoyZ),video::SColor(255,lightR,lightG,lightB),lightStart,lightEnd,lightRange, lightSequence));
             }
         }
+    }
 }
 
 void Buoys::update(irr::f32 deltaTime, irr::f32 scenarioTime, irr::core::vector3df viewPosition)
