6ad7a52590eb94974c5f4bd8411adfbfa7e43372
James Packer <james@bridgecommand.co.uk>
2016-02-09 18:33:54 +0000

    Add structure for multiplayer hub to start from

---
+++ multiplayerHub/main.cpp
@@ -0,0 +1,73 @@
+/*   Bridge Command 5.0 Ship Simulator
+     Copyright (C) 2016 James Packer
+
+     This program is free software; you can redistribute it and/or modify
+     it under the terms of the GNU General Public License version 2 as
+     published by the Free Software Foundation
+
+     This program is distributed in the hope that it will be useful,
+     but WITHOUT ANY WARRANTY; without even the implied warranty of
+     MERCHANTABILITY Or FITNESS For A PARTICULAR PURPOSE.  See the
+     GNU General Public License For more details.
+
+     You should have received a copy of the GNU General Public License along
+     with this program; if not, write to the Free Software Foundation, Inc.,
+     51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA. */
+
+// main.cpp
+
+#include <iostream>
+
+// Include the Irrlicht header
+#include "irrlicht.h"
+#include "../Utilities.hpp"
+
+//Mac OS:
+#ifdef __APPLE__
+#include <mach-o/dyld.h>
+#endif
+
+int main()
+{
+
+    //Mac OS:
+	#ifdef __APPLE__
+    //Find starting folder
+    char exePath[1024];
+    uint32_t pathSize = sizeof(exePath);
+    std::string exeFolderPath = "";
+    if (_NSGetExecutablePath(exePath, &pathSize) == 0) {
+        std::string exePathString(exePath);
+        size_t pos = exePathString.find_last_of("\\/");
+        if (std::string::npos != pos) {
+            exeFolderPath = exePathString.substr(0, pos);
+        }
+    }
+    //change up from BridgeCommand.app/Contents/MacOS/mh.app/Contents/MacOS to BridgeCommand.app/Contents/Resources
+    exeFolderPath.append("/../../../../Resources");
+    //change to this path now, so ini file is read
+    chdir(exeFolderPath.c_str());
+    //Note, we use this again after the createDevice call
+	#endif
+
+    //User read/write location - look in here first and the exe folder second for files
+    std::string userFolder = Utilities::getUserDir();
+
+    std::cout << "User folder is " << userFolder <<std::endl;
+
+    /*Overview:
+    Load scenario, including initial positions of each player
+    Start as an enet client
+    Connect to each multiplayer pc (each as a server)
+    Send scenario information to each multiplayer pc (Tailored to include all other players as other ships)
+    Send first update to each pc, based on initial positions
+    Then loop:
+        Get feedback from each pc for current position and heading
+        Use this to update internal model
+        Send out update to each pc, including other ship positions
+    */
+
+
+    return(0);
+
+}
