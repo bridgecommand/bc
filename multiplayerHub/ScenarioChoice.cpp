ef74f7c9238bd393a3391e88d3f3f1fa2d0c7a5d
James Packer <james@bridgecommand.co.uk>
2016-04-24 09:35:30 +0100

    Add pause before loading multiplayer scenario to avoid spurious unflushed key events

--- multiplayerHub/ScenarioChoice.cpp
+++ multiplayerHub/ScenarioChoice.cpp
@@ -70,6 +70,10 @@ void ScenarioChoice::chooseScenario(std::string& scenarioName, std::string& host
     //select list box as active, so user can use up/down arrows without needing to click
     gui->setFocus(scenarioListBox);
 
+    //Flush old key/clicks etc, with a 0.2s pause
+    device->sleep(200);
+    device->clearSystemMessages();
+
     //Link to our event receiver
     StartupEventReceiver startupReceiver(scenarioListBox,GUI_ID_SCENARIO_LISTBOX,GUI_ID_OK_BUTTON);
     device->setEventReceiver(&startupReceiver);
