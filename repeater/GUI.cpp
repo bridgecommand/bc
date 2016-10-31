300b0cc5946ec8ec099562f7a0f6bbcdf1bf985b
James Packer <james@bridgecommand.co.uk>
2016-10-31 07:33:57 +0000

    Show compass rose as gyro repeater

--- repeater/GUI.cpp
+++ repeater/GUI.cpp
@@ -49,7 +49,64 @@ GUIMain::~GUIMain()
 void GUIMain::updateGuiData(irr::f32 time, irr::f32 ownShipHeading)
 {
 
-    heading->setHeading(ownShipHeading);
+    bool showHeadingIndicator = false;
+
+    if (showHeadingIndicator) {
+        heading->setHeading(ownShipHeading);
+        heading->setVisible(true);
+    } else {
+
+        heading->setVisible(false);
+
+        //draw compass rose
+        s32 centreX = device->getVideoDriver()->getScreenSize().Width/2;
+        s32 centreY = device->getVideoDriver()->getScreenSize().Height/2;
+        s32 radius = device->getVideoDriver()->getScreenSize().Height*0.45;
+
+        video::IVideoDriver* driver = device->getVideoDriver();
+
+        driver->draw2DPolygon(core::vector2d<s32>(centreX,centreY),radius,video::SColor(255,255,255,255),60);
+
+        for(unsigned int i = 0; i<360; i++) {
+            //Draw compass bearings
+            f32 xVector = sin(core::degToRad(i-ownShipHeading));
+            f32 yVector = -1*cos(core::degToRad(i-ownShipHeading));
+
+            //set scale of line
+            f32 lineStart;
+            bool printAngle = false;
+            if (i%45==0) {
+                lineStart = 0.7;
+            } else if (i%10==0) {
+                lineStart = 0.85;
+                printAngle = true;
+            } else if (i%5==0) {
+                lineStart = 0.88;
+            } else {
+                lineStart = 0.9;
+            }
+
+            s32 startX = centreX + lineStart*xVector*radius;
+            s32 endX = centreX + 1.0*xVector*radius;
+            s32 startY = centreY + lineStart*yVector*radius;
+            s32 endY = centreY + 1.0*yVector*radius;
+
+            driver->draw2DLine(core::vector2d<s32>(startX,startY),core::vector2d<s32>(endX,endY),video::SColor(255,255,255,255));
+            if (printAngle) {
+                s32 textWidth = guienv->getSkin()->getFont()->getDimension(core::stringw(i).c_str()).Width;
+                s32 textHeight = guienv->getSkin()->getFont()->getDimension(core::stringw(i).c_str()).Height;
+                s32 textStartX = centreX + 0.8*xVector*radius-0.5*textWidth;
+                s32 textEndX = textStartX+textWidth;
+                s32 textStartY = centreY + 0.8*yVector*radius-0.5*textHeight;
+                s32 textEndY = textStartY+textHeight;
+                guienv->getSkin()->getFont()->draw(core::stringw(i),core::rect<s32>(textStartX,textStartY,textEndX,textEndY),video::SColor(255,255,255,255));
+            }
+        }
+
+
+        //Draw heading indicator
+        driver->draw2DLine(core::vector2d<s32>(centreX,centreY),core::vector2d<s32>(centreX,centreY-1.1*radius),video::SColor(255,255,255,255));
+    }
     guienv->drawAll();
 
 }
