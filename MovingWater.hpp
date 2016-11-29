9c6965514363191b8c958fc3bf69d77a6c3dbf3a
James Packer <james@bridgecommand.co.uk>
2016-11-29 07:40:34 +0000

    Updating copyright notices

--- MovingWater.hpp
+++ MovingWater.hpp
@@ -1,8 +1,23 @@
-//Based on CWaterSurfaceSceneNode
-
+//Based on CWaterSurfaceSceneNode, with the original copyright notice:
 // Copyright (C) 2002-2012 Nikolaus Gebhardt
 // This file is part of the "Irrlicht Engine".
 // For conditions of distribution and use, see copyright notice in irrlicht.h
+
+/*   Bridge Command 5.0 Ship Simulator
+     Copyright (C) 2014 James Packer
+
+     This program is free software; you can redistribute it and/or modify
+     it under the terms of the GNU General Public License version 2 as
+     published by the Free Software Foundation
+
+     This program is distributed in the hope that it will be useful,
+     but WITHOUT ANY WARRANTY; without even the implied warranty of
+     MERCHANTABILITY Or FITNESS For A PARTICULAR PURPOSE.  See the
+     GNU General Public License For more details.
+
+     You should have received a copy of the GNU General Public License along
+     with this program; if not, write to the Free Software Foundation, Inc.,
+     51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA. */
 
 #ifndef __MOVING_WATER_HPP_INCLUDED__
 #define __MOVING_WATER_HPP_INCLUDED__
