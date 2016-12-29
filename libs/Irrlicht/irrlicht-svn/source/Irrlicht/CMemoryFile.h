7200dc85fc9043d993e48fad2c4d3486f865bf8a
James Packer <james@bridgecommand.co.uk>
2016-12-29 20:20:38 +0000

    Add git add Irrlicht from SVN at r5355

---
+++ libs/Irrlicht/irrlicht-svn/source/Irrlicht/CMemoryFile.h
@@ -0,0 +1,95 @@
+// Copyright (C) 2002-2012 Nikolaus Gebhardt
+// This file is part of the "Irrlicht Engine".
+// For conditions of distribution and use, see copyright notice in irrlicht.h
+
+#ifndef __C_MEMORY_READ_FILE_H_INCLUDED__
+#define __C_MEMORY_READ_FILE_H_INCLUDED__
+
+#include "IReadFile.h"
+#include "IWriteFile.h"
+#include "irrString.h"
+
+namespace irr
+{
+
+namespace io
+{
+
+	/*!
+		Class for reading from memory.
+	*/
+	class CMemoryReadFile : public IReadFile
+	{
+	public:
+
+		//! Constructor
+		CMemoryReadFile(const void* memory, long len, const io::path& fileName, bool deleteMemoryWhenDropped);
+
+		//! Destructor
+		virtual ~CMemoryReadFile();
+
+		//! returns how much was read
+		virtual size_t read(void* buffer, size_t sizeToRead) _IRR_OVERRIDE_;
+
+		//! changes position in file, returns true if successful
+		virtual bool seek(long finalPos, bool relativeMovement = false) _IRR_OVERRIDE_;
+
+		//! returns size of file
+		virtual long getSize() const _IRR_OVERRIDE_;
+
+		//! returns where in the file we are.
+		virtual long getPos() const _IRR_OVERRIDE_;
+
+		//! returns name of file
+		virtual const io::path& getFileName() const _IRR_OVERRIDE_;
+
+	private:
+
+		const void *Buffer;
+		long Len;
+		long Pos;
+		io::path Filename;
+		bool deleteMemoryWhenDropped;
+	};
+
+	/*!
+		Class for writing to memory.
+	*/
+	class CMemoryWriteFile : public IWriteFile
+	{
+	public:
+
+		//! Constructor
+		CMemoryWriteFile(void* memory, long len, const io::path& fileName, bool deleteMemoryWhenDropped);
+
+		//! Destructor
+		virtual ~CMemoryWriteFile();
+
+		//! returns how much was written
+		virtual size_t write(const void* buffer, size_t sizeToWrite) _IRR_OVERRIDE_;
+
+		//! changes position in file, returns true if successful
+		virtual bool seek(long finalPos, bool relativeMovement = false) _IRR_OVERRIDE_;
+
+		//! returns where in the file we are.
+		virtual long getPos() const _IRR_OVERRIDE_;
+
+		//! returns name of file
+		virtual const io::path& getFileName() const _IRR_OVERRIDE_;
+
+		virtual bool flush() _IRR_OVERRIDE_;
+
+	private:
+
+		void *Buffer;
+		long Len;
+		long Pos;
+		io::path Filename;
+		bool deleteMemoryWhenDropped;
+	};
+
+} // end namespace io
+} // end namespace irr
+
+#endif
+
---
+++ libs/Irrlicht/irrlicht-svn/source/Irrlicht/CSoftwareTexture2.h
@@ -0,0 +1,117 @@
+// Copyright (C) 2002-2012 Nikolaus Gebhardt / Thomas Alten
+// This file is part of the "Irrlicht Engine".
+// For conditions of distribution and use, see copyright notice in irrlicht.h
+
+#ifndef __C_SOFTWARE_2_TEXTURE_H_INCLUDED__
+#define __C_SOFTWARE_2_TEXTURE_H_INCLUDED__
+
+#include "SoftwareDriver2_compile_config.h"
+
+#include "ITexture.h"
+#include "IRenderTarget.h"
+#include "CImage.h"
+
+namespace irr
+{
+namespace video
+{
+
+class CBurningVideoDriver;
+
+/*!
+	interface for a Video Driver dependent Texture.
+*/
+class CSoftwareTexture2 : public ITexture
+{
+public:
+
+	//! constructor
+	enum eTex2Flags
+	{
+		GEN_MIPMAP	= 1,
+		IS_RENDERTARGET	= 2,
+		NP2_SIZE	= 4,
+	};
+	CSoftwareTexture2(IImage* surface, const io::path& name, u32 flags);
+
+	//! destructor
+	virtual ~CSoftwareTexture2();
+
+	//! lock function
+	virtual void* lock(E_TEXTURE_LOCK_MODE mode, u32 level, u32 layer)
+	{
+		if (Flags & GEN_MIPMAP)
+		{
+			MipMapLOD = level;
+			Size = MipMap[MipMapLOD]->getDimension();
+			Pitch = MipMap[MipMapLOD]->getPitch();
+		}
+
+		return MipMap[MipMapLOD]->getData();
+	}
+
+	//! lock function
+	virtual void* lock(E_TEXTURE_LOCK_MODE mode = ETLM_READ_WRITE, u32 layer = 0) _IRR_OVERRIDE_
+	{
+		return lock(mode, 0, layer);
+	}
+
+	//! unlock function
+	virtual void unlock() _IRR_OVERRIDE_
+	{
+	}
+
+	//! Returns the size of the largest mipmap.
+	f32 getLODFactor( const f32 texArea ) const
+	{
+		return OrigImageDataSizeInPixels * texArea;
+		//return MipMap[0]->getImageDataSizeInPixels () * texArea;
+	}
+
+	//! returns unoptimized surface
+	virtual CImage* getImage() const
+	{
+		return MipMap[0];
+	}
+
+	//! returns texture surface
+	virtual CImage* getTexture() const
+	{
+		return MipMap[MipMapLOD];
+	}
+
+	virtual void regenerateMipMapLevels(void* data = 0, u32 layer = 0) _IRR_OVERRIDE_;
+
+private:
+	f32 OrigImageDataSizeInPixels;
+
+	CImage * MipMap[SOFTWARE_DRIVER_2_MIPMAPPING_MAX];
+
+	u32 MipMapLOD;
+	u32 Flags;
+	ECOLOR_FORMAT OriginalFormat;
+};
+
+/*!
+interface for a Video Driver dependent render target.
+*/
+class CSoftwareRenderTarget2 : public IRenderTarget
+{
+public:
+	CSoftwareRenderTarget2(CBurningVideoDriver* driver);
+	virtual ~CSoftwareRenderTarget2();
+
+	virtual void setTexture(const core::array<ITexture*>& texture, ITexture* depthStencil) _IRR_OVERRIDE_;
+
+	ITexture* getTexture() const;
+
+protected:
+	CBurningVideoDriver* Driver;
+};
+
+
+} // end namespace video
+} // end namespace irr
+
+#endif
+
