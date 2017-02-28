5ffa8d1337981a1b4d4b17a69fe874d38f4d79bc
James Packer <james@bridgecommand.co.uk>
2017-02-28 08:05:33 +0000

    Added a high resolution sky map from https://blenderartists.org/forum/showthread.php?24038-Free-high-res-skymaps-(Massive-07-update!)

--- Sky.cpp
+++ Sky.cpp
@@ -23,7 +23,7 @@ Sky::Sky(irr::scene::ISceneManager* smgr)
 {
     irr::video::IVideoDriver* driver = smgr->getVideoDriver();
     driver->setTextureCreationFlag(video::ETCF_CREATE_MIP_MAPS, false);
-    skyNode=smgr->addSkyDomeSceneNode(driver->getTexture("media/sky.bmp"),16,8,0.95f,2.0f,3.5*M_IN_NM); //Fixme: Range should probably be dependent on fog & camera range.
+    skyNode=smgr->addSkyDomeSceneNode(driver->getTexture("media/Sky_horiz_9.jpg"),16,8,0.95f,2.0f,3.5*M_IN_NM); //Fixme: Range should probably be dependent on fog & camera range.
     driver->setTextureCreationFlag(video::ETCF_CREATE_MIP_MAPS, true);
     skyNode->setMaterialFlag(video::EMF_FOG_ENABLE, true);
     skyNode->setMaterialFlag(video::EMF_LIGHTING, true); //Turn on lighting, so the sky gets dark as ambient light goes down
