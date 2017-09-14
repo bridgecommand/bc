9086c968b76def407e8f793f4c8f7a0c6117bd87
James Packer <james@bridgecommand.co.uk>
2017-09-14 14:48:45 +0100

    Remove duplicated build command

--- codeBlocksBuildAll.bat
+++ codeBlocksBuildAll.bat
@@ -4,5 +4,4 @@
 "c:\Program Files (x86)\CodeBlocks\codeblocks.exe" /na /nd --no-splash-screen --rebuild iniEditor\iniEditor.cbp
 "c:\Program Files (x86)\CodeBlocks\codeblocks.exe" /na /nd --no-splash-screen --rebuild launcher\launcher.cbp
 "c:\Program Files (x86)\CodeBlocks\codeblocks.exe" /na /nd --no-splash-screen --rebuild multiplayerHub\MultiplayerHub.cbp
-"c:\Program Files (x86)\CodeBlocks\codeblocks.exe" /na /nd --no-splash-screen --rebuild controller\controller.cbp
 "c:\Program Files (x86)\CodeBlocks\codeblocks.exe" /na /nd --no-splash-screen --rebuild repeater\repeater.cbp
