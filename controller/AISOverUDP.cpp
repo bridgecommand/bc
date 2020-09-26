3b51306e67bd870ba6b434b7e66aa75f9a80314c
James Packer <james@bridgecommand.co.uk>
2020-09-26 20:31:13 +0100

    Use lower level UDP code to allow for timeouts

--- controller/AISOverUDP.cpp
+++ controller/AISOverUDP.cpp
@@ -24,37 +24,33 @@ AISOverUDP::AISOverUDP(int port) {
 
 void AISOverUDP::AISThread()
 {
-    //int port = 35678; //TODO: Remove hardcoding
     asio::io_context io_context;
-    asio::ip::udp::socket socket(io_context, asio::ip::udp::endpoint(asio::ip::udp::v4(), _port));
+    asio::ip::udp::socket socket(io_context);
+    socket.open(asio::ip::udp::v4());
+    socket.bind(asio::ip::udp::endpoint(asio::ip::udp::v4(), _port));
 
     std::cout << "Set up on port " << _port << std::endl;
 
     for (;;)
     {
       try {
-        asio::ip::udp::endpoint remote_endpoint;
-        std::vector<char> buffer;
+        
+        unsigned char buf[128];
 
         std::cout << "Waiting for data 1." << std::endl;
-
-        // Wait for data to arrive on socket
-        socket.receive_from(asio::null_buffers(), remote_endpoint);
-
-        std::cout << "Waiting for data 2." << std::endl;
-
-        // Get datagram size
-        decltype(socket)::bytes_readable readable(true);
-        socket.io_control(readable);
-        auto length = readable.get();
-
-        std::cout << "Waiting for data 3." << std::endl;
-
-        // Read the datagram
-        buffer.resize(length); // Assume buffer is a std::vector<char>
-        socket.receive_from(asio::buffer(buffer.data(), buffer.size()),remote_endpoint);
-        std::string receivedString(buffer.begin(), buffer.end());
-        std::cout << "Received: " << receivedString << std::endl;
+        
+        //This is all with low level socket functions, not actually asio. Needs to be checked on Windows!
+        
+        //Set timeout
+        struct timeval tv = { 1, 0 };
+        setsockopt(socket.native_handle(), SOL_SOCKET, SO_RCVTIMEO, &tv, sizeof(tv));
+        
+        //Read from UDP socket
+        ssize_t nread = ::read(socket.native_handle(), buf, sizeof(buf));
+        
+        if (nread>0) {
+          std::cout << "Received: " << buf << std::endl;
+        }
 
       } catch (std::exception& e) {
         std::cerr << e.what() << std::endl;
