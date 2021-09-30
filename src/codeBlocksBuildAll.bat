d71b89479d3541908b6674e5025b8ddd6c1c3870
James Packer <james@bridgecommand.co.uk>
2021-09-30 12:32:47 +0100

    Minor code blocks related updates

--- src/codeBlocksBuildAll.bat
+++ src/codeBlocksBuildAll.bat
@@ -5,3 +5,4 @@
 "c:\Program Files (x86)\CodeBlocks\codeblocks.exe" /na /nd --no-splash-screen --rebuild launcher\launcher.cbp
 "c:\Program Files (x86)\CodeBlocks\codeblocks.exe" /na /nd --no-splash-screen --rebuild multiplayerHub\MultiplayerHub.cbp
 "c:\Program Files (x86)\CodeBlocks\codeblocks.exe" /na /nd --no-splash-screen --rebuild repeater\repeater.cbp
+pause
