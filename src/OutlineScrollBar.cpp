1746d2c7b7e48aa8c423960f858c80d348f4f111
James Packer <james@bridgecommand.co.uk>
2021-11-18 18:34:46 +0000

    Set location of numbers on rudder control bar

--- src/OutlineScrollBar.cpp
+++ src/OutlineScrollBar.cpp
@@ -291,7 +291,7 @@ void OutlineScrollBar::draw()
                 endPoint.Y   = 0.4*AbsoluteRect.UpperLeftCorner.Y + 0.6*AbsoluteRect.LowerRightCorner.Y;
 
                 if (ticIndicators.size() == shortTicMarks.size() && font ) {
-                    font->draw(irr::core::stringw(ticIndicators[i]),irr::core::rect<irr::s32>(startPoint.X - 100, AbsoluteRect.UpperLeftCorner.Y + 0.5*AbsoluteRect.getHeight(), startPoint.X + 100, AbsoluteRect.LowerRightCorner.Y),video::SColor(skinAlpha,0,0,0),true,false,&AbsoluteRect);
+                    font->draw(irr::core::stringw(ticIndicators[i]),irr::core::rect<irr::s32>(startPoint.X - 100, AbsoluteRect.LowerRightCorner.Y - font->getDimension(irr::core::stringw(ticIndicators[i]).c_str()).Height, startPoint.X + 100, AbsoluteRect.LowerRightCorner.Y),video::SColor(skinAlpha,0,0,0),true,false,&AbsoluteRect);
                 }
             }
             else
