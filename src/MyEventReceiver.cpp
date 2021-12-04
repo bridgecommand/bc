2f0b5cc3fa943ff38f94c1bb09103e96676fa444
James Packer <james@bridgecommand.co.uk>
2021-12-04 07:20:56 +0000

    Debugging for odd behaviour on right click

--- src/MyEventReceiver.cpp
+++ src/MyEventReceiver.cpp
@@ -179,7 +179,9 @@
 // DEE capture the wheel
               if (id == GUIMain::GUI_ID_WHEEL_SCROLL_BAR)
                   {
+                        //This sometimes gets called when we right click elsewhere! Why?!
                         model->setWheel(((irr::gui::IGUIScrollBar*)event.GUIEvent.Caller)->getPos());
+                        //std::cout << "Caller: " << event.GUIEvent.Caller <<  " getPos() is " << ((irr::gui::IGUIScrollBar*)event.GUIEvent.Caller)->getPos() << std::endl;
                   }
 // DEE capture the wheel
 
