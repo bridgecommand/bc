90b78ad886ab889e2e216583d97c7f181c727c4f
James Packer <james@bridgecommand.co.uk>
2024-09-16 21:50:39 +0100

    Built with MinGW64 and signed.

--- src/codeBlocksBuildAll.bat
+++ src/codeBlocksBuildAll.bat
@@ -1,8 +1,8 @@
-"c:\Program Files\CodeBlocks\codeblocks.exe" /na /nd --no-splash-screen --rebuild BridgeCommand.cbp
-"c:\Program Files\CodeBlocks\codeblocks.exe" /na /nd --no-splash-screen --rebuild controller\controller.cbp
-"c:\Program Files\CodeBlocks\codeblocks.exe" /na /nd --no-splash-screen --rebuild editor\editor.cbp
-"c:\Program Files\CodeBlocks\codeblocks.exe" /na /nd --no-splash-screen --rebuild iniEditor\iniEditor.cbp
-"c:\Program Files\CodeBlocks\codeblocks.exe" /na /nd --no-splash-screen --rebuild launcher\launcher.cbp
-"c:\Program Files\CodeBlocks\codeblocks.exe" /na /nd --no-splash-screen --rebuild multiplayerHub\MultiplayerHub.cbp
-"c:\Program Files\CodeBlocks\codeblocks.exe" /na /nd --no-splash-screen --rebuild repeater\repeater.cbp
+"c:\Program Files\CodeBlocks\codeblocks.exe" /na /nd --no-splash-screen --rebuild --target="Windows64" BridgeCommand.cbp
+"c:\Program Files\CodeBlocks\codeblocks.exe" /na /nd --no-splash-screen --rebuild --target="Windows64" controller\controller.cbp
+"c:\Program Files\CodeBlocks\codeblocks.exe" /na /nd --no-splash-screen --rebuild --target="Windows64" editor\editor.cbp
+"c:\Program Files\CodeBlocks\codeblocks.exe" /na /nd --no-splash-screen --rebuild --target="Windows64" iniEditor\iniEditor.cbp
+"c:\Program Files\CodeBlocks\codeblocks.exe" /na /nd --no-splash-screen --rebuild --target="Windows64" launcher\launcher.cbp
+"c:\Program Files\CodeBlocks\codeblocks.exe" /na /nd --no-splash-screen --rebuild --target="Windows64" multiplayerHub\MultiplayerHub.cbp
+"c:\Program Files\CodeBlocks\codeblocks.exe" /na /nd --no-splash-screen --rebuild --target="Windows64" repeater\repeater.cbp
 pause
