7fa4d7f6b0b2ef736e033b22924105939097c220
C/NA/RCH - Florent RICHARD <florent.richard@supmaritime.fr>
2024-12-13 14:17:07 +0100

    Add detail texture to terrain

--- src/Water.cpp
+++ src/Water.cpp
@@ -57,6 +57,11 @@ void Water::load(irr::scene::ISceneManager* smgr, irr::scene::ISceneNode* ownShi
 
     smgr->getRootSceneNode()->addChild(waterNode);
     waterNode->setMaterialTexture(0, driver->getTexture("media/water.bmp"));
+
+    //waterNode->setMaterialTexture(1, driver->getTexture("media/water2.jpg"));
+
+    waterNode->setMaterialType(irr::video::EMT_TRANSPARENT_ALPHA_CHANNEL);
+
 }
 
 void Water::update(irr::f32 tideHeight, irr::core::vector3df viewPosition, irr::u32 lightLevel, irr::f32 weather)
