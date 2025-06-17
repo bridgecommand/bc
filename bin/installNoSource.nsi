8bca30844425ecb7ce63a99fb495a71aa2c6b8c2
James Packer <james@bridgecommand.co.uk>
2025-06-17 21:10:46 +0100

    Update version to 5.10.3-alpha.1

--- bin/installNoSource.nsi
+++ bin/installNoSource.nsi
@@ -1,7 +1,7 @@
 ;setup names
 !define PROGRAMNAME "Bridge Command 5.10"
 !ifndef OUTPUTFILE
-!define OUTPUTFILE "..\BridgeCommand5.10.2.exe"
+!define OUTPUTFILE "..\BridgeCommand5.10.3-alpha.1.exe"
 !endif
 !define INSTALLLOCATION "Bridge Command 5.10"
 !define SMFOLDER "Bridge Command 5.10"
--- src/Constants.hpp
+++ src/Constants.hpp
@@ -44,7 +44,7 @@ const irr::f32 DEG_IN_RAD = 180.0 / PI;
 const irr::f32 RAD_PER_S_IN_DEG_PER_MINUTE = 180.0/PI * 60 ;
 
 //general definitions
-const std::string LONGNAME = "Bridge Command 5.10.2";
+const std::string LONGNAME = "Bridge Command 5.10.3-alpha.1";
 const std::string VERSION = "5.10";
-const std::string LONGVERSION = "5.10.2";
+const std::string LONGVERSION = "5.10.3-alpha.1";
 #endif
